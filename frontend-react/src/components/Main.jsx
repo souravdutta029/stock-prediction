2932a57bfc13397a1ed0283680879324ac39f5ae
Sourav Dutta <duttasourav029@gmail.com>
2024-12-10 00:18:53 +0530

    Homepage

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="p-5 text-center bg-light-dark rounded">
+          <h1 className='text-light'>Stock Prediction App</h1>
+          <p className="text-light lead">
+            Stock prediction leverages historical data, machine learning, and market trends to forecast future stock prices. 
+            Analysts use algorithms, sentiment analysis, and technical indicators to identify patterns. Accurate predictions 
+            empower investors to make informed decisions, though market volatility and external factors can pose challenges 
+            to precision.
+          </p>
+          <Button text="Login" class="btn btn-info" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Main
